a363d4f3f96b2cf046a4651025e4ac61fec10e3c
joannalange <joasialange@gmail.com>
2016-03-15 11:00:06 +0100

    add make_corvar

--- aln_quality/html_handler.py
+++ aln_quality/html_handler.py
@@ -45,3 +45,41 @@ def write_html(aln, wrong_cols, outname):
         template_fmt = a.read()
     with open(outname + ".html", 'w') as out:
         out.write(template_fmt.format(css_full_path, outtxt))
+
+
+def seq(num_aln, aa_aln, full_seq, max_lengths, wrong):
+    aa_aln_corvar = make_corvar(full_seq, num_aln)
+    aln_length = len(aa_aln)
+    for seq_id, seq in aa_aln.iteritems():
+        html_sequence = "{}    ".format(seq_id)
+        for r, res in enumerate(seq):
+            if res != "-" and res != " ":
+                if r in wrong[seq_id].keys():
+                    level = get_level(wrong[seq_id][r], aln_length)
+                    new_res = "<span class=featWRONG{}>{}</span>".format(level,
+                                                                         res)
+                else:
+                    new_res = "<span class=featOK>{}</span>".format(res)
+            else:
+                new_res = res
+            html_sequence += new_res
+    return html_sequence
+
+
+def make_corvar(full_seq, num_aln):
+    corvar_aln = {seq_id: {"cores": [], "vars": []} for
+                  seq_id in num_aln.keys()}
+    for seq_id, seq in num_aln.iteritems():
+        for c in seq["cores"]:
+            new_core = ""
+            for res in c:
+                new_res = full_seq[seq_id][res - 1]
+                new_core += new_res
+            corvar_aln[seq_id]["cores"].append(new_core)
+        for c in seq["vars"]:
+            new_var = ""
+            for res in c:
+                new_res = full_seq[seq_id][res - 1]
+                new_var += new_res
+            corvar_aln[seq_id]["vars"].append(new_var)
+    return corvar_aln
---
+++ tests/unit/test_html_handler.py
@@ -0,0 +1,33 @@
+from nose.tools import eq_
+
+from aln_quality.html_handler import make_corvar
+
+
+def test_make_corvar():
+    aa_aln = {'1': "ABCDEFGHIJKLMNOP"}
+    num_aln = {
+        '1': {
+            'cores': [
+                [1, 2],
+                [6, 7, 8],
+                [9, 10, 11]
+            ],
+            'vars': [[3, 4, 5],
+                     [12, 13, 14, 15, 16]
+                     ]}}
+
+    expected = {
+        '1': {
+            'cores': [
+                "AB",
+                "FGH",
+                "IJK"
+            ],
+            'vars': [
+                "CDE",
+                "LMNOP"
+            ]}}
+
+    res = make_corvar(aa_aln, num_aln)
+
+    eq_(res, expected)
