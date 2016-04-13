a5cc6a62ae27fa1eda99f4f356315b3bfc63c47b
joannalange <joasialange@gmail.com>
2016-04-13 15:59:58 +0200

    checks in the integration test, add TODOs

--- gold_standard_src/tests/integration/test_calc_alignment_quality.py
+++ gold_standard_src/tests/integration/test_calc_alignment_quality.py
@@ -5,14 +5,18 @@ from gold_standard_src.calc_alignment_quality import calculate_aln_quality
 
 
 @patch('gold_standard_src.gold_standard.parsers.fasta.open', mock_open(
-    read_data=">ID11A\nA-C\nDEF\n>ID12B\nGHI\n"), create=True)
+    read_data=">ID11A\nA-C\nDEF\n>ID12B\nABCGHI\n"), create=True)
 @patch('gold_standard_src.gold_standard.num_seq.open', mock_open(
-    read_data="ID11B, A-C DEF \nID12B, GHI"), create=True)
+    read_data="ID11A, A-C DEF \nID12B, ABC GHI"), create=True)
 @patch('gold_standard_src.gold_standard.parsers.var_file.open', mock_open(
-    read_data="ID11B, 0 A-C 0 DEF 0\nID12B, 0 GHI 0"), create=True)
+    read_data="ID11A, 0 A-C 0 DEF 0\nID12B, 0 ABC 0 GHI 0"), create=True)
 @patch('gold_standard_src.gold_standard.parsers.fasta.os.path.exists')
 @patch('gold_standard_src.calc_alignment_quality.process_results')
 def test_calc_alignment_quality_multi(mock_process_results, mock_path_exists):
+    # TODO: make a real test
+    # TODO: finish the dummy test & make it pass
+
+    # dummy test to check if all the mocks work
     # setup mocks
     mock_path_exists.return_value = True
     mock_process_results.return_value = 0
@@ -29,13 +33,45 @@ def test_calc_alignment_quality_multi(mock_process_results, mock_path_exists):
     multi = True
 
     # expected call to process_results
-    expected = [
+    expected_call = [
         {},                                    # pairwise score matrix
         {"TP": 0, "FP": 0, "FN": 0, "TN": 0},  # confusion matrix
         {},                                    # SP scores
         'testpath'                             # output path
     ]
 
-    calculate_aln_quality(input_paths, output, input_format, multi)
-    process_res_calls = list(mock_process_results.call_args[0])
-    eq_(process_res_calls, expected)
+    expected_result = {
+        'num_aln': {
+            'var': {
+                'ID11A': [],
+                'ID12B': []
+            },
+            'cores': {
+                'ID11A': [1, '-', 2, 3, 4, 5],
+                'ID12B': [1, 2, 3, 4, 5, 6]
+            }
+        },
+        'full': {
+            'ID11A': 'ACDEF',
+            'ID12B': 'ABCGHI'
+
+        },
+        'aa_aln': {
+            'ID11A': 'A-CDEF',
+            'ID12B': 'ABCGHI'
+        },
+        'wrong_cols': {
+            'ID11A': {},
+            'ID12B': {}
+        },
+        'core_indexes': None
+    }
+
+    calc_result = calculate_aln_quality(input_paths, output, input_format, multi)
+    process_res_call = list(mock_process_results.call_args[0])
+    # eq_(process_res_call, expected_call)
+    # eq_(calc_result['wrong_cols'], expected_result['wrong_cols'])
+    eq_(calc_result['aa_aln'], expected_result['aa_aln'])
+    eq_(calc_result['num_aln'], expected_result['num_aln'])
+    eq_(calc_result['full'], expected_result['full'])
+    eq_(calc_result['core_indexes'], expected_result['core_indexes'])
