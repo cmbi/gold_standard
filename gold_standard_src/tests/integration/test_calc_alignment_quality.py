cfebb4b474eacb884ee129fbf72d945358c3e76f
joannalange <joasialange@gmail.com>
2016-04-15 15:16:05 +0200

    extend the integration test

--- gold_standard_src/tests/integration/test_calc_alignment_quality.py
+++ gold_standard_src/tests/integration/test_calc_alignment_quality.py
@@ -5,17 +5,18 @@ from gold_standard_src.calc_alignment_quality import calculate_aln_quality
 
 
 @patch('gold_standard_src.gold_standard.parsers.fasta.open', mock_open(
-    read_data=">ID11A\nA-C\nDEF\n>ID12B\nABCGHI\n"), create=True)
+    read_data=">ID11A\nA-C\nDEF\n>ID12B\nABCGHI\n>ID13C\nACDFGH\n"),
+       create=True)
 @patch('gold_standard_src.gold_standard.num_seq.open', mock_open(
-    read_data="ID11A, 0 A-C 0 DEF 0\nID12B, 0 ABC 0 GHI 0"), create=True)
+    read_data="ID11A, 0 A-C 0 DEF 0\nID12B, 0 ABC 0 GHI 0\nID13C, "
+    "0 ACD 0 FGH 0\n"), create=True)
 @patch('gold_standard_src.gold_standard.parsers.var_file.open', mock_open(
-    read_data="ID11A, 0 A-C 0 DEF 0\nID12B, 0 ABC 0 GHI 0"), create=True)
+    read_data="ID11A, 0 A-C 0 DEF 0\nID12B, 0 ABC 0 GHI 0\nID13C, "
+    "0 ACD 0 FGH 0"), create=True)
 @patch('gold_standard_src.gold_standard.parsers.fasta.os.path.exists')
 @patch('gold_standard_src.calc_alignment_quality.process_results')
 def test_calc_alignment_quality_multi(mock_process_results, mock_path_exists):
-    # TODO: make a real test
-
-    # dummy test to check if all the mocks work
+    # DUMMY TEST
     # setup mocks
     mock_path_exists.return_value = True
     mock_process_results.return_value = 0
@@ -35,10 +36,18 @@ def test_calc_alignment_quality_multi(mock_process_results, mock_path_exists):
     expected_call = [
         {   # pairwise confusion matrices
             frozenset(['ID11A', 'ID12B']):
-                {"TP": 10, "FP": 0, "FN": 0, "TN": 1}
+                {"TP": 10, "FP": 0, "FN": 0, "TN": 1},
+            frozenset(['ID11A', 'ID13C']):
+                {"TP": 10, "FP": 0, "FN": 0, "TN": 1},
+            frozenset(['ID13C', 'ID12B']):
+                {"TP": 12, "FP": 0, "FN": 0, "TN": 0}
         },
-        {"TP": 10, "FP": 0, "FN": 0, "TN": 1},  # overall confusion matrix
-        {frozenset(['ID11A', 'ID12B']): 1.0},  # SP scores; 1.0 is max
+        {"TP": 32, "FP": 0, "FN": 0, "TN": 2},  # overall confusion matrix
+        {
+            frozenset(['ID11A', 'ID12B']): 1.0,
+            frozenset(['ID11A', 'ID13C']): 1.0,
+            frozenset(['ID13C', 'ID12B']): 1.0,
+        },  # SP scores; 1.0 is max
         'testpath'                             # output path
     ]
 
@@ -46,25 +55,30 @@ def test_calc_alignment_quality_multi(mock_process_results, mock_path_exists):
         'num_aln': {
             'var': {
                 'ID11A': [],
-                'ID12B': []
+                'ID12B': [],
+                'ID13C': []
             },
             'cores': {
                 'ID11A': [1, '-', 2, 3, 4, 5],
-                'ID12B': [1, 2, 3, 4, 5, 6]
+                'ID12B': [1, 2, 3, 4, 5, 6],
+                'ID13C': [1, 2, 3, 4, 5, 6]
             }
         },
         'full': {
             'ID11A': 'ACDEF',
-            'ID12B': 'ABCGHI'
+            'ID12B': 'ABCGHI',
+            'ID13C': 'ACDFGH'
 
         },
         'aa_aln': {
             'ID11A': 'A-CDEF',
-            'ID12B': 'ABCGHI'
+            'ID12B': 'ABCGHI',
+            'ID13C': 'ACDFGH'
         },
         'wrong_cols': {
             'ID11A': {},
-            'ID12B': {}
+            'ID12B': {},
+            'ID13C': {}
         },
         'core_indexes': None
     }
