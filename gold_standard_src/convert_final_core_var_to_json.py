cad3ffc539d9535ad4a0f74b3dc62dc8db633aa0
Joanna Lange <joasialange@gmail.com>
2017-12-06 16:44:41 +0100

    WIP; convert_corvar_to_json_alignment

--- gold_standard_src/convert_final_core_var_to_json.py
+++ gold_standard_src/convert_final_core_var_to_json.py
@@ -27,15 +27,39 @@ def convert_corvar_to_json_alignment(corvar_data):
         #   'u': unaligned
         #   'd' don't know
         #   'm1', 'm2', ..., 'mN' - multiple solutions
-        score_modifiers: {'a': 1, 'u': -1, , 'd': 0, 'm0': 0, 'm1': 0.1, 'm2': 0.2}
+        score_modifiers: {'a': 1, 'u': -1, 'd': 0, 'm0': 0, 'm1': 0.1, 'm2': 0.2}
         }
         alignments: {
             strct_id_1: {
+                # residue number 1 from strct 1
+                #  should be aligned with residue 2 from the target seq
+                1: {2: 'a'},
 
+                # residue number 2 from strct 1
+                #  can be aligned with residue 3 (for score m1) from the target seq
+                #  or residue 4 (for score m2)
+                2: {3: 'm1', 4: 'm2'},
+
+                # give the "unaligned" penalty score if residue 3 is aligned with anything
+                3: {'*': 'u'},
+
+                # give the "don't know" penalty score if residue 4 is aligned with anything
+                4: {'*': 'd'},
             }
         }
-
     """
+    score_modifiers = {'a': 1, 'u': -1, 'd': 0, 'm0': 0, 'm1': 0.1, 'm2': 0.2}
+
+    # get all structure ids
+    alignments = {strct_id: {} for strct_id in corvar_data["alns"]["cores"]}
+    core_alns = corvar_data["alns"]["cores"]
+    var_regions = corvar_data["alns"]["var"]
+
+    # for strct_id, core_aln in corvar_data["alns"]["cores"].iteritems():
+    result = {
+        "alignments": alignments, "target": corvar_data["target"], "score_modifiers": score_modifiers
+    }
+    return result
 
 
 def run_convert(inpath, outpath):
