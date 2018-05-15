ecf8389efb5284c64fcd5c8857caf34cfe0418e6
joannalange <joasialange@gmail.com>
2018-05-15 17:26:45 +0200

    Allow diff ids

--- gold_standard_src/gold_standard/parsers/aln3SSP.py
+++ gold_standard_src/gold_standard/parsers/aln3SSP.py
@@ -12,10 +12,17 @@ def parse_3SSP(aln_path):
     strcts_order = []
     for l in infile:
         seq_id = ''.join(l.split()[0:2])
+        seq_start = 2
         if len(seq_id) != 5:
             seq_id = l.split()[0]
+            seq_start = 1
         strcts_order.append(seq_id)
-        sequence = l.split()[2].replace('?', '-')
+        # sequence = l.split()[2].replace('?', '-')
+        sequence = l.split()[seq_start:]
+        seq_with_lower = [
+            core[0].lower() + core[1:-1] + core[-1].lower() for core in sequence
+        ]
+        sequence = "".join(seq_with_lower)
         seq_dict[seq_id] = sequence
     # remove gap-only sequences
 
