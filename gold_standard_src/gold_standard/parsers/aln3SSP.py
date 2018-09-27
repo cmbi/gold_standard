101177973aae4fb0f8c84ad38d4eb42620b27074
joannalange <joasialange@gmail.com>
2018-09-27 16:45:56 +0200

    Skip empty lines in parse3SSP

--- gold_standard_src/gold_standard/parsers/aln3SSP.py
+++ gold_standard_src/gold_standard/parsers/aln3SSP.py
@@ -10,20 +10,22 @@ def parse_3SSP(aln_path):
         infile = a.read().splitlines()
     seq_dict = {}
     strcts_order = []
-    for l in infile:
-        seq_id = ''.join(l.split()[0:2])
+    for line in infile:
+        if not line:
+            # skip empty lines
+            continue
+
+        seq_id = ''.join(line.split()[0:2])
         seq_start = 2
         if len(seq_id) != 5:
-            seq_id = l.split()[0]
+            seq_id = line.split()[0]
             seq_start = 1
         strcts_order.append(seq_id)
-        # sequence = l.split()[2].replace('?', '-')
-        sequence = l.split()[seq_start:]
+        sequence = line.split()[seq_start:]
         seq_with_lower = [
             core[0].lower() + core[1:-1] + core[-1].lower() for core in sequence
         ]
         sequence = "".join(seq_with_lower)
         seq_dict[seq_id] = sequence
-    # remove gap-only sequences
 
     return seq_dict, strcts_order
